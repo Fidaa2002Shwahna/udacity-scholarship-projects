09c93e342e7cbed27b933337776599d24401c905
Fidaa2002Shwahna <programmerfidaa@gmail.com>
2024-10-14 14:37:24 +0300

    Removed folder from repository

--- travel-app/babel.config.cjs
+++
@@ -1,4 +0,0 @@
-  //  JavaScript compiler. Babel converts modern JavaScript code into a version that is compatible with older browsers or environments. This process is necessary because not all JavaScript features are supported in all environments.
-module.exports = {
-  presets: ['@babel/preset-env'],
-};
